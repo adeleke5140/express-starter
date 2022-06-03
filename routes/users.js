a013b6828bcf947f3be6cbe31e0061bfd201a02f
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-03 01:38:55 +0100

    add routes path

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,31 @@
-app.get("/users", (req, res) => {
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
   res.send("User List")
 })
 
-app.get("/users/new", (req, res) => {
+router.get("/new", (req, res) => {
   res.send("User New Form")
 })
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    //id is only declared in this function scope
+
+    const { id } = req.params
+
+    if (id == 12) {
+      res.send("🧑‍🦰: user is the 12th person on this site")
+    }
+    res.send(`🧑‍🦰: I am the user with an id of ${id}`)
+  })
+  .put((req, res) => {
+    res.send(`Update user with the ID ${id}`)
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with the ID of ${id}`)
+  })
+
+module.exports = router
