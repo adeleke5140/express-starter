97a8ebcc5033eb32e40f5b0e2c9ffaa67bf090e0
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-03 02:14:19 +0100

    experiment with query string to api endpoints

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
+  const { name } = req.query
+
+  res.send(`🧑‍🦰I am the user with the name of ${name}`)
   res.send("User List")
 })
 
@@ -9,22 +12,25 @@ router.get("/new", (req, res) => {
   res.send("User New Form")
 })
 
+const users = [{ name: "Kehinde" }, { name: "Sally" }]
+router.param("id", (req, res, next, id) => {
+  req.user = users[id]
+  next()
+})
+
 router
   .route("/:id")
   .get((req, res) => {
-    //id is only declared in this function scope
-
-    const { id } = req.params
-
-    if (id == 12) {
-      res.send("🧑‍🦰: user is the 12th person on this site")
-    }
-    res.send(`🧑‍🦰: I am the user with an id of ${id}`)
+    res.send(`🧑‍🦰: I am the user with a name of ${id}`)
   })
   .put((req, res) => {
+    const { id } = req.params
+
     res.send(`Update user with the ID ${id}`)
   })
   .delete((req, res) => {
+    const { id } = req.params
+
     res.send(`Delete user with the ID of ${id}`)
   })
 
